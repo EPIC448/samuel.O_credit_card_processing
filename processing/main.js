b54102c4a0f928be9e51509402d0a2c9628f309c
EPIC448 <samrey2018@gmail.com>
2021-05-20 08:08:41 -0400

    latest update

--- processing/main.js
+++ processing/main.js
@@ -1,16 +1,22 @@
+
 import CreditRecordsCollection from '../utils/collection.js';
 import { default as get_data } from '../utils/data.js'
 import { default as format_data } from '../utils/data.js'
-import { default as variable } from '../utils/data.js'
+
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 
 let name
-
-if (name = '') {
+ console.log('In main.js file')
+if (require.main === module) {
     data = get_data()
     accounts, transactions = format_data(data = data)
 
     let credit_data = CreditRecordsCollection(accounts = accounts)
     credit_data.process(transactions = transactions)
 
-    console.log(credit_data)
+    process.stdout.write(credit_data); 
+
+    // console.log(credit_data)
 }
